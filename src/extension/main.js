1f986a854a090b39c8d5217f560e849e1d253ee2
Evan Jones <evanjones125@gmail.com>
2023-02-22 15:29:32 -0500

    add ability to parse through DOM using Tree Walker

--- src/extension/main.js
+++ src/extension/main.js
@@ -1,6 +1,30 @@
 // should access code of website in the current window
 
 // tests for console
-console.log('Hi');
+// console.log('Hii');
 
-console.log(document.querySelectorAll('*'));
+// const tree = document.querySelectorAll('*');
+
+// console.log(tree[1]);
+// console.log(tree);
+
+const walker = document.createTreeWalker(
+  document.documentElement,
+  NodeFilter.SHOW_ELEMENT
+);
+
+const islands = [];
+
+while (walker.nextNode()) {
+  let current = walker.currentNode;
+  if (current.tagName === 'ASTRO-ISLAND') {
+    console.log(
+      current.tagName,
+      [...current.attributes].map(({value,name}) => `${name}=${value}`).join()
+    );
+
+    islands.push([...current.attributes].map(({value,name}) => `${name}=${value}`).join());
+  }
+}
+
+console.log(islands[0].substring(islands[0].indexOf('client=') + 7, islands[0].indexOf('client=') + 11));
