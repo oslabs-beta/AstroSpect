55009fd2a7e5473b3e84b2415cfe161d9ac44a93
Nicholas Park <npark009@gmail.com>
2023-02-23 12:22:29 -0800

    Prettify TargetHTML - unfinished

--- src/extension/main.js
+++ src/extension/main.js
@@ -8,30 +8,30 @@
 // console.log(tree[1]);
 // console.log(tree);
 
-const walker = document.createTreeWalker(
-  document.documentElement,
-  NodeFilter.SHOW_ELEMENT
-);
+// const walker = document.createTreeWalker(
+//   document.documentElement,
+//   NodeFilter.SHOW_ELEMENT
+// );
 
-const islands = [];
+// const islands = [];
 
-while (walker.nextNode()) {
-  let current = walker.currentNode;
-  if (current.tagName === 'ASTRO-ISLAND') {
-    console.log(
-      current.tagName,
-      [...current.attributes]
-        .map(({ value, name }) => `${name}=${value}`)
-        .join()
-    );
+// while (walker.nextNode()) {
+//   let current = walker.currentNode;
+//   if (current.tagName === 'ASTRO-ISLAND') {
+//     console.log(
+//       current.tagName,
+//       [...current.attributes]
+//         .map(({ value, name }) => `${name}=${value}`)
+//         .join()
+//     );
 
-    islands.push(
-      [...current.attributes]
-        .map(({ value, name }) => `${name}=${value}`)
-        .join()
-    );
-  }
-}
+//     islands.push(
+//       [...current.attributes]
+//         .map(({ value, name }) => `${name}=${value}`)
+//         .join()
+//     );
+//   }
+// }
 // // Define an async function to retrieve the target page's HTML
 // async function getTargetPageHTML() {
 //   console.log('Attempting to retrieve dom tree from target html');
@@ -58,14 +58,14 @@ while (walker.nextNode()) {
 //     const html = await getTargetPageHTML();
 //     const domTreeContainer = document.getElementById('dom-tree');
 //     domTreeContainer.innerHTML = html;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
+// //   } catch (error) {
+// //     console.error(error);
+// //   }
+// // })();
 
-console.log(
-  islands[0].substring(
-    islands[0].indexOf('client=') + 7,
-    islands[0].indexOf('client=') + 11
-  )
-);
+// console.log(
+//   islands[0].substring(
+//     islands[0].indexOf('client=') + 7,
+//     islands[0].indexOf('client=') + 11
+//   )
+// );
