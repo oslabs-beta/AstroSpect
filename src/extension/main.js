17aee840d28ac2e97946ad8e6835d43aadd17ed8
Nicholas Park <npark009@gmail.com>
2023-02-23 07:46:39 -0800

    target html render - test

--- src/extension/main.js
+++ src/extension/main.js
@@ -1,6 +1,30 @@
-// should access code of website in the current window
+// // Define an async function to retrieve the target page's HTML
+// async function getTargetPageHTML() {
+//   console.log('Attempting to retrieve dom tree from target html');
+//   // Use chrome.devtools.inspectedWindow.eval() to retrieve the page's HTML
+//   const html = await new Promise((resolve, reject) => {
+//     chrome.devtools.inspectedWindow.eval(
+//       'document.documentElement.outerHTML',
+//       (result, exception) => {
+//         if (exception) {
+//           reject(exception);
+//         } else {
+//           resolve(result);
+//         }
+//       }
+//     );
+//   });
 
-// tests for console
-console.log('Hi');
+//   return html;
+// }
 
-console.log(document.querySelectorAll('*'));
+// // Call the getTargetPageHTML() function and render the HTML in the dev tools panel
+// (async () => {
+//   try {
+//     const html = await getTargetPageHTML();
+//     const domTreeContainer = document.getElementById('dom-tree');
+//     domTreeContainer.innerHTML = html;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
