b1778bddae81755c6a6853941f5bad7b8ca32642
Evan Jones <evanjones125@gmail.com>
2023-03-09 19:08:02 -0500

    change panel display message for static html

--- src/app/containers/SidePane.jsx
+++ src/app/containers/SidePane.jsx
@@ -41,7 +41,9 @@ const SidePane = (props) => {
       {/* // when component is not astro island */}
       {!currentComp && (
         <>
-          <p>No hydrated component selected</p>
+          <h3>Type: </h3>
+          <p>Static HTML</p>
+          <hr />
         </>
       )}
 
