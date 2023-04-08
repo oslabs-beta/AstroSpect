4e86f4bebb257b442ec52f96d069085298f526cf
Nicholas Park <npark009@gmail.com>
2023-04-07 19:33:18 -0700

    ElementView ts conversion

--- src/app/components/ElementView.tsx
+++ src/app/components/ElementView.tsx
@@ -4,14 +4,14 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { ElementViewProps } from '../types';
 
-const ElementView: React.FC = (props: ElementViewProps): JSX.Element => {
+const ElementView: React.FC<ElementViewProps> = (props): JSX.Element => {
   const { handleClick, expanded, handleToggle, elementData } = props;
 
   // returns the completed tree
   return (
     <>
       <TreeView
-        aria-label="file system navigator"
+        aria-label='file system navigator'
         defaultCollapseIcon={<ExpandMoreIcon sx={{ color: '#d5bcef' }} />}
         defaultExpandIcon={<ChevronRightIcon sx={{ color: '#d5bcef' }} />}
         onNodeSelect={handleClick}
